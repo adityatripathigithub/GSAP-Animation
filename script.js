70704eaf931c0e7b71483205f3538c9966ff2730
adity tripathi <tripathiaditya163@gmail.com>
2024-04-02 23:53:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+gsap.from("nav h1",{
+    y:-50,
+    opacity:0,
+    duration:1,
+    
+})
+
+
+
+gsap.from("#part2 h3",{
+    y : -50,
+    opacity:0,
+    duration:1,
+    stagger:0.2,
+    delay: 0.8
+})
